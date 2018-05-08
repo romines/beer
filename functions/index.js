96cc237e7402c01d918622d995488579f5288116
Adam Romines <adam.romines@gmail.com>
2018-05-08 11:43:40 -0600

    Adds (optional) stripping of empty fields to http endpoint cloud function

--- functions/index.js
+++ functions/index.js
@@ -6,23 +6,43 @@ const generateThumbnail = require('./generateThumbnail');
 
 exports.httpEndpoint = functions.https.onRequest((request, response) => {
   const resortId = request.query.r;
+  const STRIP_EMPTY = !!(request.query.strip && request.query.strip === '1');
+  console.log(`STRIP_EMPTY: ${STRIP_EMPTY}`);
   const docRef = db.doc('resorts/' + resortId);
+  let _deleteCount = 0;
+
   const rmUuid = (contact) => {
     if (contact.id !== undefined) delete contact.id;
     return contact
   }
-  const stripContactIds = (group) => {
-    group.list = group.list.map(rmUuid)
+  const stripContactIdsAndEmptyFields = (group) => {
+    group.list = group.list
+      .map(rmUuid)
+      .map(stripEmptyFields)
     return group
   }
+  const stripEmptyFields = (contact) => {
+    if (STRIP_EMPTY) {
+      Object.keys(contact).forEach(key => {
+        if ((contact[key]) !== 0 && !contact[key]) {
+          delete contact[key]
+          _deleteCount += 1
+        }
+      });
+    }
+    console.log(`${_deleteCount} fields deleted . . .`);
+    return contact
+  }
+
   docRef.get().then(function(doc) {
     let resortData = doc.data()
-    resortData.contactGroups = resortData.contactGroups.map(stripContactIds)
+    resortData.contactGroups = resortData.contactGroups.map(stripContactIdsAndEmptyFields)
     let responseString = JSON.stringify(resortData)
     response.send(responseString);
   }).catch(function(error) {
     response.send(error);
   });
+
 });
 
 exports.generateThumbnail = generateThumbnail;
