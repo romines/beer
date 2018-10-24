3953368c821310652051fb085d59894c1383e95f
Adam Romines <adam.romines@gmail.com>
2018-10-23 21:33:56 -0600

    Hides LocationSelector if resort has no mapFiles

--- src/components/EditContact.vue
+++ src/components/EditContact.vue
@@ -169,6 +169,7 @@
     </div>
 
     <location-selector
+      v-if="this.$store.state.resortMeta.mapFiles.length"
       :coordinate-string="localState.contact.rect"
       :map-id="localState.contact.mapId"
       @coordinateClick="onCoordinateClick"
