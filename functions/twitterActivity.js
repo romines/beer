fa4f2fad75a42a936bc3e734836ccde654e0d848
Adam Romines <adam.romines@gmail.com>
2018-11-21 17:43:48 -0700

    Updates twitter endpoint to exclude replies

--- functions/twitterActivity.js
+++ functions/twitterActivity.js
@@ -13,10 +13,10 @@ module.exports = functions.https.onRequest((request, response) => {
 
   const resortId = request.query.r;
 
-  database.ref(`${resortId}/lastTweet`).once('value')
+  database.ref(`${resortId}/twitter`).once('value')
 
     .then(snapshot => {
-      return parseTweet(snapshot.val(), resortId);
+      return parseTwitterData(snapshot.val(), resortId);
     })
     .then(lastTweet => {
       response.send(JSON.stringify(lastTweet));
@@ -27,22 +27,27 @@ module.exports = functions.https.onRequest((request, response) => {
 
 });
 
-function parseTweet(cachedTwitterData, resortId) {
+function parseTwitterData(twitterData, resortId) {
 
   if (process.env.GCLOUD_PROJECT !== 'resorts-tapped-admin') return Promise.resolve('Tweets not fetched in non-production environments . . .');
-  if (!cachedTwitterData || !cachedTwitterData.user || !cachedTwitterData.user.screen_name) return Promise.resolve(`Twitter cache not configured for resortId ${resortId}`);
-  if (isFresh(cachedTwitterData)) return Promise.resolve(cachedTwitterData);
+  if (!twitterData || !twitterData.screenName) return Promise.resolve(`Twitter cache not configured for resortId ${resortId}`);
+  if (isFresh(twitterData.lastTweet)) return Promise.resolve(twitterData.lastTweet);
 
-  // screen_name populated in firebase manually (before first tweet can be retrieved),
-  // subsequently retrieved from cache to avoid extra resort meta lookup
+  // screenName must be added to firebase rtdb manually (before first tweet can be retrieved)
 
-  const screen_name = cachedTwitterData.user.screen_name;
+  const screen_name = twitterData.screenName;
 
   return new Promise((resolve, reject) => {
 
-    twitter.get('statuses/user_timeline', { screen_name, count: 1}, (err, response) => {
+    twitter.get('statuses/user_timeline', { screen_name, count: 20, exclude_replies: true }, (err, response) => {
       if (err) return reject(err);
-      const lastTweet = response[0];
+
+      let lastTweet;
+      if (!response[0] && twitterData.lastTweet) {
+        lastTweet = twitterData.lastTweet;
+      } else {
+        lastTweet = response[0];
+      }
 
       updateCache(resortId, lastTweet).then(lastTweet => {
         return resolve(lastTweet);
@@ -53,19 +58,20 @@ function parseTweet(cachedTwitterData, resortId) {
 
 }
 
+
 function updateCache(resortId, tweet) {
   const tweetData = {
     ...tweet,
     fetched: moment().format()
   };
-  const ref = database.ref(`${resortId}/lastTweet`);
+  const ref = database.ref(`${resortId}/twitter/lastTweet`);
   return ref.set(tweetData).then(() => {
     return Promise.resolve(tweetData);
   });
 }
 
 function isFresh(tweet) {
-  if (!tweet.text) return false;
+  if (!tweet || !tweet.text) return false;
   const fetched = moment(tweet.fetched);
   const now = moment();
   return now.diff(fetched, 'minutes') < 10;
